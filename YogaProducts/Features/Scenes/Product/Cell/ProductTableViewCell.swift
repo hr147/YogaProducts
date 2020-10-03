8cbd29330da8be12a95a852e25f2017b140d7045
Haroon Ur Rasheed <haroon-ur-rasheed@live.com>
2020-10-03 14:01:53 +0200

    design is completed for both orientations

---
+++ YogaProducts/Features/Scenes/Product/Cell/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  YogaProducts
+//
+//  Created by Haroon Ur Rasheed on 02/10/2020.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
